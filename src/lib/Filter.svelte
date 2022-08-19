abb64dfbaf4cf4d3fcad92fe33bdae69931aacac
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-19 20:41:35 +0100

    created FIlter component

---
+++ src/lib/Filter.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    const filter = e => {
+        dispatch('filter', e.target.value)
+    }
+</script>
+
+<div class="ml-4 mt-4">
+    <label for="user-filter">Filter User</label>
+    <select 
+        name="user-filter" id="user-filter" 
+        class="border rounded-lg px-4 py-2 ml-4"
+        on:change={filter}
+    >
+        <option value="all">All</option>
+        <option value="active">Active</option>
+        <option value="inactive">Inactive</option>
+    </select>
+</div>
