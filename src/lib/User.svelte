f0825be378bb45733cc5def72f003cd3d9731688
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-19 20:42:04 +0100

    created User component including remove functionality

---
+++ src/lib/User.svelte
@@ -0,0 +1,34 @@
+<script>
+import { createEventDispatcher } from "svelte";
+
+
+    export let user;
+    const { id, image, name, email } = user;
+    const dispatch = createEventDispatcher();
+    const removeUser = (id) => {
+        dispatch('remove', id)
+    }
+</script>
+
+{#if user}
+<div 
+    class="
+        flex m-5 bg-white border p-2 rounded-lg 
+        shadow-md hover:shadow-lg cursor-pointer 
+        items-center relative
+    "
+>
+    <span 
+        on:click={() => removeUser(user.id)}
+        class="
+            absolute right-0 top-0 -mt-3 rounded-full 
+            w-5 h-5 text-center bg-red-300 text-white 
+            text-sm hover:bg-red-700
+        "
+    >X</span>
+    <span class="w-10">{id}</span>
+    <img src={image} alt={name} class="rounded-full w-10 h-10"/>
+    <p class="mx-4 w-20">{name}</p>
+    <p class="mx-4 w-20">{email}</p>
+</div>
+{/if}
