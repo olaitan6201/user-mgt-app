b61d09dba7603ad881f6afb250fd77820963c3ab
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-20 07:22:27 +0100

    modified Users component

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -5,6 +5,7 @@ import user2 from "../assets/images/user2.png"
 import user3 from "../assets/images/user3.png"
 import userHack from "../assets/images/user-hack.png"
 import Filter from "./Filter.svelte";
+import NewUser from "./NewUser.svelte";
 
     let users = [
         {
@@ -56,7 +57,10 @@ import Filter from "./Filter.svelte";
 <div>
     <h1 class="text-2xl text-center mt-10">List of Users</h1>
     
-    <Filter on:filter={filterUsers} />
+    <div class="flex justify-between mx-4 items-center">
+        <Filter on:filter={filterUsers} />
+        <NewUser />
+    </div>
     
     {#each filteredUsers as user, i (user.id)}
     <User {user} on:remove={removeUser}/>
