74839e95bd99ed0c75e83f00e532ee37b09d464b
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-20 11:10:38 +0100

    added Routing to App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,27 @@
 <script>
-import Users from "./lib/Users.svelte";
+import { Router, Route, Link } from "svelte-navigator";
+import HomePage from "./pages/HomePage.svelte";
+import UsersPage from "./pages/UsersPage.svelte";
 </script>
 
 <main>
-  <Users />
+
+  <Router>
+    <nav class="p-4 bg-blue-800 text-white">
+      <Link class="mx-4 cursor-pointer hover:underline" to="/">Home</Link>
+      <Link class="mx-4 cursor-pointer hover:underline" to="/users">Users</Link>
+    </nav>
+
+    <div>
+      <Route path="/">
+        <HomePage />
+      </Route>
+      <Route path="/users">
+        <UsersPage />
+      </Route>
+    </div>
+  </Router>
+
 </main>
 
 <style>
