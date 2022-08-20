5e95098a26d93eedba91a398f61d9432b86cb895
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-20 11:10:17 +0100

    created HomePage component

---
+++ src/pages/HomePage.svelte
@@ -0,0 +1,7 @@
+<div class="flex h-screen">
+    <div class="m-auto text-center">
+        <h1 class="text-2xl border border-2 border-black p-2">Welcome to </h1>
+    
+        <h4 class="text-4xl mt-4">Svelte Users Mgt. App</h4>
+    </div>
+</div>
