350ae86b8415873c13372a46a9ec4e0f2d339c51
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2022-08-20 09:23:24 +0100

    tweened and cubicIn (motion, easing)

--- src/lib/Users.svelte
+++ src/lib/Users.svelte
@@ -1,30 +1,42 @@
 <script>
 import User from "./User.svelte";
-import user1 from "../assets/images/user1.png"
-import user2 from "../assets/images/user2.png"
-import user3 from "../assets/images/user3.png"
-import userHack from "../assets/images/user-hack.png"
 import Filter from "./Filter.svelte";
 import NewUser from "./NewUser.svelte";
 import { createUser, removeUser, users } from '../store'
+import { tweened } from 'svelte/motion'
+import { cubicIn } from "svelte/easing";
+import { onMount } from "svelte";
     
     $: filteredUsers = $users;
-
+    $: progress = tweened(0, {
+        duration: 1000,
+        easing: cubicIn
+    })
+    
     const filterUsers = e => {
         const status = e.detail
-        if(status === 'all') return filteredUsers = $users;
-        const active = status === 'active';
-        filteredUsers = $users.filter((user) => user.active === active);
+        if(status === 'all') filteredUsers = $users;
+        else {
+            const active = status === 'active';
+            filteredUsers = $users.filter((user) => user.active === active);
+        }
+        progress.set(filteredUsers.length);
     }
+
+    onMount(()=>{
+        progress.set($users.length);
+    })
 </script>
 
 <div>
     <h1 class="text-2xl text-center mt-10">List of Users</h1>
-    
+
     <div class="flex justify-between mx-4 items-center">
         <Filter on:filter={filterUsers} />
         <NewUser on:create={createUser} />
+        <progress max={$users.length} min="0" value={$progress} class="w-500 mx-4"></progress>
     </div>
+
     
     {#each filteredUsers as user, i (user.id)}
     <User {user} on:remove={removeUser}/>
